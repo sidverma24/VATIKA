62a8f2c5e11e02fd6d396a332e85d985a71f7f63
Siddharth Verma <sidverma2408@gmail.com>
2022-05-28 21:14:29 +0530

    inital UI commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonDatetime } from '@ionic/angular';
+import { format, parseISO, getDate, getMonth, getYear } from 'date-fns';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  @ViewChild(IonDatetime, { static: true }) datetime: IonDatetime;
+  dateValue = '';
+  dateValue2 = '';
   constructor() {}
-
+  formatDate(value: string) {
+    return format(parseISO(value), 'MMM dd yyyy');
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonDatetime } from '@ionic/angular';
+import { format, parseISO, getDate, getMonth, getYear } from 'date-fns';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+@ViewChild(IonDatetime, { static: true }) datetime: IonDatetime;
+dateValue = '';
+dateValue2 = '';
   constructor() {}
 
+  confirm() {
+    // this.datetime.nativeEl.confirm();
+  }
+
+  reset() {
+    // this.datetime.nativeEl.reset();
+  }
+
+  formatDate(value: string) {
+    return format(parseISO(value), 'MMM dd yyyy');
+  }
+
+  isDateEnabled(dateIsoString: string) {
+    const date = new Date(dateIsoString);
+    if (getDate(date) === 1 && getMonth(date) === 0 && getYear(date) === 2022) {
+      // Disables January 1, 2022.
+      return false;
+    }
+    return true;
+  }
 }
